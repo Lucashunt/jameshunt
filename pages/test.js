d9bd866bc0f1bad9555eebf085b253234dd0a87f
Lucas Ingemann Hunt <lucashunt1@gmail.com>
2022-12-21 14:03:05 +0100

    changes

--- pages/test.js
+++ pages/test.js
@@ -1,7 +1,7 @@
 const test = () => {
     return (
 <>
-<a title="Add to Calendar" class="addeventstc" data-id="dJ541766" href="https://www.addevent.com/calendar/dJ541766" target="_blank" rel="noreferrer">Add to Calendar</a>
+<a title="Add to Calendar" className="addeventstc" data-id="dJ541766" href="https://www.addevent.com/calendar/dJ541766" target="_blank" rel="noreferrer">Add to Calendar</a>
 	<script type="text/javascript" src="https://cdn.addevent.com/libs/stc/1.0.2/stc.min.js" async defer></script>
 </>
 
