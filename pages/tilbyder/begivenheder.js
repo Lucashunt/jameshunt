49dc22f23e733674d7218436239b28f10de6c26c
Lucas Ingemann Hunt <lucashunt1@gmail.com>
2023-01-12 13:56:01 +0100

    hej

--- pages/tilbyder/begivenheder.js
+++ pages/tilbyder/begivenheder.js
@@ -2,6 +2,7 @@ import { Disclosure } from "@headlessui/react";
 import { ChevronUpIcon } from "@heroicons/react/20/solid";
 import Header from "../../comps/Header";
 import Link from "next/link";
+import axios from "axios";
 
 function Dis({ p }) {
   return (
@@ -74,8 +75,37 @@ function Generelt({ pris, dato, tid, adresse, tilmelding, overskrift, p }) {
   );
 }
 
-const Begivenheder = () => {
+export async function getServerSideProps({ }) {
+
+
+
+    const client = axios.create({
+      baseURL: 'https://cdn.contentful.com',
+      headers: {
+        'Content-Type': "application/json",
+        'Authorization': `Bearer ${process.env.API_KEY}`
+      }
+    })
     
+
+
+    const response = await client.get('/spaces/v9sb7460yx5u/entries')
+      const events = response.data.items
+    
+      
+
+    return {
+        props: {
+            events,
+           
+        }, 
+      };
+}
+
+const Begivenhedertest = (events) => {
+    console.log("data", events )
+  
+
   return (
     <div className="bg-background">
         <Header title="Begivenheder" description="Events page"/>
@@ -85,89 +115,27 @@ const Begivenheder = () => {
         </h1>
         <div className="grid sm:grid-cols-3 gap-20">
 
+           
 
+        {events.events.map((event) => ( 
+            <>
+          
           <Generelt
-            pris="295"
-            dato="Torsdag d. 29 December"
-            tid="Kl. 15 - 17:30"
-            adresse="Nørretoftevej 20, 3200 Helsinge"
-            tilmelding="Du er tilmeldt når du har bekræftet din deltagelse på Facebook, sms eller mail, og indbetalt til mobilepay nr.: 42 53 11 04. Mærk betaling med dato for workshoppen"
-            overskrift="En stille dag Helsinge"
-            p={<p>2,5 timers Yin og Dynamic yoga. Der vil være fokus på at trække tempoet ud, invitere langsomheden ind og være i stilhed. Øvelserne vil fordrer at kroppen detoxer - både fysisk og mentalt. <br /><br />Denne workshop indeholder en mere meditativ yogaform, med fordybelsen i yogaens dybere stillinger og åndedrætstræning.<br /><br />Dynamic yoga er en yogaform, der har fokus på mindfulness og fordybelse gennem blide flow.<br /><br />Yin yoga er en blid og meditativ træning, der lader kroppen komme ind i en dyb oplevelse af afslapning og ro. Med ro i krop og sind skaber du restitution, større bevægelsesfrihed og du bliver opmærksom. Du bliver guidet ind i stillingerne med fokus på åndedrættet, stillingerne i Yin holdes afslappet i 3-5 min, hvor du opnår dybe og rolige stræk, der både smidiggør muskler, led og bindevæv.<br /><br />Du får mulighed for at vende blikket indad for en stund og være i stilhed, både fysisk og mentalt.</p>}
+            pris={event.fields.pris}
+            dato={ new Date(event.fields.dato).toLocaleDateString([], { weekday: "long", month: 'long', day: 'numeric' })}
+            tid={event.fields.tid}
+            adresse={event.fields.adresse}
+            tilmelding={event.fields.tilmelding}
+            overskrift={event.fields.overskrift}
+            p={<p>{event.fields.p.content[0].content[0].value}</p>}
           />
-                    <Generelt
-            pris="295"
-            dato="Torsdag d. 30 December"
-            tid="Kl. 10 - 12:30"
-            adresse="Hovedgade 19, 3250 Gilleje"
-            tilmelding="Du er tilmeldt når du har bekræftet din deltagelse på Facebook, sms eller mail, og indbetalt til mobilepay nr.: 42 53 11 04. Mærk betaling med dato for workshoppen"
-            overskrift="En stille dag Gilleleje"
-            p={<p>2,5 timers Yin og Dynamic yoga. Der vil være fokus på at trække tempoet ud, invitere langsomheden ind og være i stilhed. Øvelserne vil fordrer at kroppen detoxer - både fysisk og mentalt. <br /><br />Denne workshop indeholder en mere meditativ yogaform, med fordybelsen i yogaens dybere stillinger og åndedrætstræning.<br /><br />Dynamic yoga er en yogaform, der har fokus på mindfulness og fordybelse gennem blide flow.<br /><br />Yin yoga er en blid og meditativ træning, der lader kroppen komme ind i en dyb oplevelse af afslapning og ro. Med ro i krop og sind skaber du restitution, større bevægelsesfrihed og du bliver opmærksom. Du bliver guidet ind i stillingerne med fokus på åndedrættet, stillingerne i Yin holdes afslappet i 3-5 min, hvor du opnår dybe og rolige stræk, der både smidiggør muskler, led og bindevæv.<br /><br />Du får mulighed for at vende blikket indad for en stund og være i stilhed, både fysisk og mentalt.</p>}
-          />
+          </>
+        ))}
 
-          <Generelt
-            pris="575"
-            dato="Afventer"
-            tid="Afventer"
-            adresse="Nørretoftevej 20, 3200 Helsinge"
-            tilmelding="Du er tilmeldt når du har bekræftet din deltagelse på Facebook, sms eller mail, og indbetalt til mobilepay nr.: 42 53 11 04. Mærk betaling med dato for workshoppen"
-            overskrift="En kold tyrker"
-            p={<p>Er du hele tiden på jagt efter det næste fix? Har du hele tiden behov for anerkendelse. Føler du et
-                konstant behov for, at tjekke ind på de sociale medier. Fylder mad og drikke meget i din bevidsthed,
-                eller har du bare lyst til at arbejde 24/7.
-                Driften får os frem, sørger for at vi står op om morgenen, og får os til at handle, spise ect. Den del af
-                vores hjerne hvor vores drift tager sit udgangspunkt, hedder hypothalamus. Den sørger for, at
-                balancere vores krop og hjerne via hormoner, det autonome nervesystem og drifter.
-                <br /> <br />
-                &quot;PETER LUND MADSEN KALDER HYPOTHALAMUS FOR “DEN STØRSTE TRUSSEL MOD DET MODERNE
-                MENNESKE&quot;
-                <br /> <br />
-                Det er således en god tjener, men en dårlig herre, da den ved ubalance meget nemt giver os en
-følelse af, at noget styre os. Konstant vil hypothalamus belønnes med arbejde, mad, drikke, hård
-sport, medier ect. Hele tiden skal vi handle for, at stille hypothalamus tilfreds. Uden disciplin er vi i
-hænderne på denne gamle del af vores hjerne.
-<br /> <br />
-På denne workshop arbejder vi i 6 timer på, at &quot;nulstille&quot; denne del af vores hjerne. Vi tager den
-nyeste og mest udviklede del af vores hjerne i brug. Den præfrontale cortex. Her sidder vores
-fornuft og empati. Her sidder evnen til, at gøre noget vi måske ikke lige har lyst til, men som på den
-lange bane gør os godt.
-<br /> <br />
-Vi giver gennem øvelser, os en pause fra ydre distraktioner, og plads til indre støj. Og får ofte en
- fornemmelse af, at blive &quot;nulstillet&quot; og igen blive herre i eget hus.
- <br /> <br />
- &quot;Det kræver en kold tyrker, at komme hjem&quot; 
- 
-                </p>}
-          />
-                    <Generelt
-            pris="295"
-            dato="Afventer"
-            tid="Kl. 10 - 12:30"
-            adresse="Nørretoftevej 20, 3200 Helsinge"
-            tilmelding="Du er tilmeldt når du har bekræftet din deltagelse på Facebook, sms eller mail, og indbetalt til mobilepay nr.: 42 53 11 04. Mærk betaling med dato for workshoppen"
-            overskrift="Skuldre, nakke, yin og blide bevægelser"
-            p={<p>På denne workshop arbejder vi med skuldre og nakke. Stress har tendens til at sætte sig i de områder med spændinger, til tider hovedpine, en følelse af træthed og svimmelhed som resultat af spændingerne. Gennem ásanas (stillinger) og åndedræt vil vi få løsnet op i muskler og sener og få skabt øget velvære. <br /><br /> Du vil få værktøjer til selv at arbejde med disse områder derhjemme.<br /><br />Workshoppen afsluttes med &quot;Yin og blide bevægelser&quot; er en yogaform, hvis sigte er at skabe balance gennem lige dele aktiv og passiv, blid og nærværende træning.<br /><br />Yin yoga er en blid og meditativ træning, der lader kroppen komme ind i en dyb oplevelse af afslapning og ro. Med ro i krop og sind skaber du restitution, større bevægelsesfrihed, og du bliver opmærksom. Du bliver guidet ind i stillingerne med fokus på åndedrættet, og stillingerne holdes afslappet i 3-5 min, hvor du opnår dybe og rolige stræk, der både smidiggør muskler, led og bindevæv.<br /><br />De blide bevægelser aktiverer musklerne, styrker din krop og hjælper til at skabe en balance mellem det passive og aktive.</p>}
-          />
-
-<Generelt
-            pris="Se pdf under læs mere"
-            dato="D. 15 - 17 September"
-            tid="Start Kl. 15"
-            adresse="Rytsebækvej 17, 4780 Stege"
-            tilmelding="Du er tilmeldt når du har bekræftet din deltagelse på Facebook, sms eller mail, og indbetalt til mobilepay nr.: 42 53 11 04. Mærk betaling med dato for workshoppen"
-            overskrift="Weekend retreat"
-            p={    <a
-              href="/files/retreat.pdf"
-              alt="alt text"
-              target="_blank"
-              rel="noopener noreferrer"
-            >Hent pdf</a>}
-          />
-
-        </div>
+        </div>       
       </div>
     </div>
   );
 };
 
-export default Begivenheder;
+export default Begivenhedertest;
